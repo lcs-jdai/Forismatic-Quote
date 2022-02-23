073d1b15dc3096eded39c749f1467f3481dfe0b2
lcs-jdai <jdai@lcs.on.ca>
2022-02-22 20:12:03 -0500

    Initial Commit

---
+++ Forismatic Quote/Forismatic_QuoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  Forismatic_QuoteApp.swift
+//  Forismatic Quote
+//
+//  Created by Jerry Dai on 2022-02-22.
+//
+
+import SwiftUI
+
+@main
+struct Forismatic_QuoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
